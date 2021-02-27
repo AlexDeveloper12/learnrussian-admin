e70b3d8870999219878e95c6fab44cebb1ad689d
Alex Biddle <alexbiddle72@gmail.com>
2021-02-27 22:31:31 +0000

    Adding more eslint changes to filterRadioButton

--- src/components/FilterRadioButton/FilterRadioButton.js
+++ src/components/FilterRadioButton/FilterRadioButton.js
@@ -2,22 +2,24 @@ import React, { useContext } from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import { AppContext } from '../../App';
-
+import propTypes from 'prop-types';
 
 const FilterRadioButton = ({ label, compValue, defaultValue }) => {
+  const { filterByPhraseType, selectedFilter } = useContext(AppContext);
 
-    const { filterByPhraseType, selectedFilter } = useContext(AppContext);
+  return (
 
-    return (
-        
-            <FormControlLabel
-                label={label}
-                control={<Radio color="primary" checked={selectedFilter === compValue ? true : false} onClick={filterByPhraseType} value={defaultValue} />}
+  <FormControlLabel
+            label={label}
+            control={<Radio color="primary" checked={selectedFilter === compValue ? true : false} onClick={filterByPhraseType} value={defaultValue} />}
+            value={defaultValue} />
+  );
+};
 
-                value={defaultValue} />
-
-    )
-
-}
+FilterRadioButton.propTypes = {
+  label: propTypes.string,
+  compValue: propTypes.string,
+  defaultValue: propTypes.string
+};
 
 export default FilterRadioButton;
